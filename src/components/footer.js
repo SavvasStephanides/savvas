a67c03e888f5d9f24571a89786aefba207a14a42
Savvas.Stephanides <savvas.stephanides@ipo.gov.uk>
2021-03-23 14:21:00 +0000

    Footer updates

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,5 +5,9 @@ export default function Header(){
   
     return <footer>
         <div className="notice sans-serif-font">2021 Savvas Stephanides</div>
+
+        <div className="notice sans-serif-font">If you're enjoying my work, <a href="https://buymeacoffee.com/SavvasStephnds">please consider buying me a coffee</a>. Thank you!</div>
+
+        <div className="notice sans-serif-font">Some icons from <a href="https://www.freepik.com/">Freepik</a></div>
     </footer>
 }
