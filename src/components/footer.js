ca0060a734fddf562ff42e1acc1a5c336017ad95
Savvas.Stephanides <savvas.stephanides@ipo.gov.uk>
2021-03-23 16:16:14 +0000

    BMC icon on footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,7 +6,7 @@ export default function Header(){
     return <footer>
         <div className="notice sans-serif-font">2021 Savvas Stephanides</div>
 
-        <div className="notice sans-serif-font">If you're enjoying my work, <a href="https://buymeacoffee.com/SavvasStephnds">please consider buying me a coffee</a>. Thank you!</div>
+        <div className="bmc"><a href="https://www.buymeacoffee.com/SavvasStephnds"><img src="/assets/bmc-blue.png" alt="Buy me a coffee"/></a></div>
 
         <div className="notice sans-serif-font">Some icons from <a href="https://www.freepik.com/">Freepik</a></div>
     </footer>
