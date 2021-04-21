1dc6eaf61d8a10944fff84a6587a37daca3c969a
Savvas.Stephanides <savvas.stephanides@ipo.gov.uk>
2021-04-21 16:26:32 +0100

    Snake game

---
+++ static/stuff/snake/precache-manifest.e45e39289c3529e3d889c08517719273.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2c16e10a88b847c0b917ea36c289329",
+    "url": "./index.html"
+  },
+  {
+    "revision": "30ef9271515cc26ffaf6",
+    "url": "./static/css/main.46cc713c.chunk.css"
+  },
+  {
+    "revision": "af626aac32f621c46641",
+    "url": "./static/js/2.501007fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.501007fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30ef9271515cc26ffaf6",
+    "url": "./static/js/main.cb7e9952.chunk.js"
+  },
+  {
+    "revision": "b02e8dd6b1e68a8e0a5a",
+    "url": "./static/js/runtime-main.47c760ce.js"
+  }
+]);
