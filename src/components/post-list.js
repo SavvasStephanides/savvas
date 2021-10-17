7e54247209f8f0ac28cb6fb460c15f0b62469334
Savvas Stephanides <savvascyp@hotmail.com>
2021-10-17 09:48:48 +0100

    Link component for post list

--- src/components/post-list.js
+++ src/components/post-list.js
@@ -1,16 +1,16 @@
 import React from "react"
 import "../style/post-list.css"
+import { Link } from "gatsby"
 
 export default function PostList({posts}){
     return <ul className="post-list">
         {posts.map(post => <li key={post.id}>
-            <a href={"/" + post.series.details.slug + "/" + post.slug}>
+            <Link to={"/" + post.series.details.slug + "/" + post.slug}>
                 <div className="thumbnail" style={{background: "url('/thumbnails/" + post.meta.thumbnail + "')"}}></div>
                 <div className="details">
                     <h3 className="serif-font">{post.meta.title}</h3>
                 </div>
-                
-            </a>
+            </Link>
         </li>)}
     </ul>
 }
